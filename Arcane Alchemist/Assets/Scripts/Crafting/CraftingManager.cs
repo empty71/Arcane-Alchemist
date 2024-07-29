f70d4a2cfed500c61cd4f7ff8268266e85a0aab7
Valentijn <220006@student.glu.nl>
2024-07-29 13:58:41 +0200

    base crafting done

---
+++ Arcane Alchemist/Assets/Scripts/Crafting/CraftingManager.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CraftingManager : MonoBehaviour
+{
+    private Item CurrentItem;
+    public Image customCursor;
+
+    public Slot[] craftingSlots;
+
+    public List<Item> itemList;
+    public string[] recipes;
+    public Item[] recipeResults;
+    public Slot resultSlot;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            if(CurrentItem != null)
+            {
+              customCursor.gameObject.SetActive(false);
+                Slot nearestSlot = null;
+                float shortestDistance = float.MaxValue;
+
+                foreach (Slot slot in craftingSlots)
+                {
+                   float dist = Vector2.Distance(Input.mousePosition, slot.transform.position);
+
+                    if (dist < shortestDistance)
+                    {
+                        shortestDistance = dist;
+                        nearestSlot = slot;
+                    }
+                }
+                nearestSlot.gameObject.SetActive(true);
+                nearestSlot.GetComponent<Image>().sprite = CurrentItem.GetComponent<Image>().sprite;
+                nearestSlot.item = CurrentItem;
+                itemList[nearestSlot.index] = CurrentItem;
+
+                CurrentItem = null;
+
+                CheckForCreatedRecipes();
+                
+            }
+        }
+    }
+
+    void CheckForCreatedRecipes()
+    {
+        resultSlot.gameObject.SetActive(false);
+        resultSlot.item = null;
+
+        string currentRecipeString = "";
+        foreach(Item item in itemList)
+        {
+            if(item != null)
+            {
+                currentRecipeString += item.itemName;
+            }
+            else
+            {
+                currentRecipeString += "null";
+            }
+        }
+        for(int i = 0; i<recipes.Length; i++)
+        {
+            if(recipes[i] == currentRecipeString)
+            {
+                resultSlot.gameObject.SetActive(true);
+                resultSlot.GetComponent<Image>().sprite = recipeResults[i].GetComponent<Image>().sprite;
+                resultSlot.item = recipeResults[i];
+            }
+        }
+    }
+
+    public void onClickSlot(Slot slot)
+    {
+        slot.item = null;
+        itemList[slot.index] = null;
+        slot.gameObject.SetActive(false);
+        CheckForCreatedRecipes();
+    }
+    public void OnmouseDownItem(Item item)
+    {
+        if(CurrentItem == null)
+        {
+            CurrentItem = item;
+            customCursor.gameObject.SetActive(true);
+            customCursor.sprite = CurrentItem.GetComponent<Image>().sprite;
+        }
+    
+    }
+}
