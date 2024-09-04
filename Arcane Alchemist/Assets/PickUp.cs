1938a5ff066ca77e1d0579db330116312506d6f5
Valentijn <220006@student.glu.nl>
2024-09-04 15:35:10 +0200

    u can pick up items

---
+++ Arcane Alchemist/Assets/PickUp.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public StatItem item;
+
+    public Inventory inventory;
+    private void Start()
+    {
+        inventory = FindObjectOfType<Inventory>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        inventory.inventoryItemList.Add(item);
+        Debug.Log("item picked up");
+    }
+}
