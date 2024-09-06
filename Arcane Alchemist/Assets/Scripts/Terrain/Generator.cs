aad0b035098b6bffe39db3086a00e747dfcfd3e6
Valentijn <220006@student.glu.nl>
2024-09-06 22:05:27 +0200

    part one prodecual gen maps

--- Arcane Alchemist/Assets/Scripts/Terrain/Generator.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Generator 
-{
-    public virtual void Generate() { }
-    public virtual void HydrolicErosion() { }
-}
